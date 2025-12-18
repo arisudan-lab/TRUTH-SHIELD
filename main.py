6d25436dfb71ca59bead34371a790039bc30fc5f
Arisudan Pradhan <pradhanarisudan@gmail.com>
2025-12-19 03:33:57 +0530

    bak

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="TruthGuard - Fake News & Deepfake Detector")
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:3000"],
+    allow_origins=["http://localhost:8080", "http://localhost:3000","truth-shield-hp1b3f7dd-arisudans-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
